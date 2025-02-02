cd23dae584746550eac0ada8a6e3536fa0d16a9a
unknown <khemanthaws@gmail.com>
2025-02-02 15:17:10 -0500

    autocid

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,15 @@ variable "subscription_id" {
   type        = string
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestorage${random_id.unique.hex}"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "openai_rg" {
   name     = "openai-rg"
   location = "East US"
@@ -15,6 +24,20 @@ resource "azurerm_resource_group" "openai_rg" {
 
 resource "random_id" "unique" {
   byte_length = 4
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstatestorage${random_id.unique.hex}"
+  resource_group_name      = azurerm_resource_group.openai_rg.name
+  location                 = azurerm_resource_group.openai_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
 }
 
 resource "azurerm_cognitive_account" "openai_account" {
