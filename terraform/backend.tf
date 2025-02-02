90cb41f61c95d5335321d1a858a2aa9e8d928925
unknown <khemanthaws@gmail.com>
2025-02-02 18:35:29 -0500

    autocid

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,5 +5,10 @@ terraform {
     storage_account_name  = "tfbackendstatic"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
+
+    client_id             = var.client_id
+    tenant_id             = var.tenant_id
+    subscription_id       = var.subscription_id
+    use_msi               = false
   }
 }
