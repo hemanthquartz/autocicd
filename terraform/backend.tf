3dbb39e008617980558b6f9b02b4a3b9f2f8195d
unknown <khemanthaws@gmail.com>
2025-02-02 18:11:04 -0500

    autocid

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "openai-rg"
-    storage_account_name  = "tfbackend${random_id.unique.hex}"
+    storage_account_name  = "tfbackendstatic"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
