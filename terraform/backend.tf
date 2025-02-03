1cd0dc4cd8c3e7cb109034760cdf2f2ea3702db1
unknown <khemanthaws@gmail.com>
2025-02-02 19:24:24 -0500

    autocid

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "openai-rg"
-    storage_account_name  = "tfbackendstatic"
+    resource_group_name   = "openai_rg"
+    storage_account_name  = "tfbackendmanual"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
--- terraform/main.tf
+++ terraform/main.tf
@@ -21,33 +21,10 @@ variable "tenant_id" {
   type        = string
 }
 
-resource "azurerm_resource_group" "openai_rg" {
-  name     = "openai-rg"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "tf_backend" {
-  name                     = "tfbackendstatic"
-  resource_group_name      = azurerm_resource_group.openai_rg.name
-  location                 = azurerm_resource_group.openai_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "tf_state" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tf_backend.name
-  container_access_type = "private"
-}
-
-resource "random_id" "unique" {
-  byte_length = 4
-}
-
 resource "azurerm_cognitive_account" "openai_account" {
   name                = "openaiaccount${random_id.unique.hex}"
-  location            = azurerm_resource_group.openai_rg.location
-  resource_group_name = azurerm_resource_group.openai_rg.name
+  location            = "East US"
+  resource_group_name = "openai_rg"
   kind                = "OpenAI"
   sku_name            = "S0"
 
@@ -56,6 +33,10 @@ resource "azurerm_cognitive_account" "openai_account" {
   network_acls {
     default_action = "Allow"
   }
+}
+
+resource "random_id" "unique" {
+  byte_length = 4
 }
 
 output "openai_endpoint" {
