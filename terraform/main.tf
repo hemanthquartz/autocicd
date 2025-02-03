2b2a0124b6075a494d4cd3e0b89fed0ccb715723
unknown <khemanthaws@gmail.com>
2025-02-02 22:00:19 -0500

    autocid

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_cognitive_account" "openai_account" {
-  name                = "openaiaccount${random_id.unique.hex}"
+  name                = "openaiaccount"
   location            = "East US"
   resource_group_name = "openai_rg"
   kind                = "OpenAI"
   sku_name            = "S0"
 
-  custom_subdomain_name = "openaiaccount${random_id.unique.hex}"
+  custom_subdomain_name = "openai"
 
   network_acls {
     default_action = "Allow"
