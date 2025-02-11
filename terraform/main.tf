f648d709cfe69274e000a226a8f7f3fe4761b13b
hemanthquartz <71367565+hemanthquartz@users.noreply.github.com>
2025-02-10 22:11:21 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
@@ -6,10 +5,19 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
+resource "random_id" "unique" {
+  byte_length = 4
+}
+
+resource "azurerm_resource_group" "openai_rg" {
+  name     = "openai_rg"
+  location = "East US"
+}
+
 resource "azurerm_cognitive_account" "openai_account" {
   name                = "openaiaccount${random_id.unique.hex}"
-  location            = "East US"
-  resource_group_name = "openai_rg"
+  location            = azurerm_resource_group.openai_rg.location
+  resource_group_name = azurerm_resource_group.openai_rg.name
   kind                = "OpenAI"
   sku_name            = "S0"
 
@@ -20,8 +28,17 @@ resource "azurerm_cognitive_account" "openai_account" {
   }
 }
 
-resource "random_id" "unique" {
-  byte_length = 4
+resource "azurerm_cognitive_deployment" "gpt4_deployment" {
+  name                 = "gpt4-deployment"
+  cognitive_account_id = azurerm_cognitive_account.openai_account.id
+  model {
+    name     = "gpt-4"
+    version  = "1106-preview"
+    format   = "OpenAI"
+  }
+  scale {
+    type = "Standard"
+  }
 }
 
 output "openai_endpoint" {
@@ -31,4 +48,12 @@ output "openai_endpoint" {
 output "openai_key" {
   value     = azurerm_cognitive_account.openai_account.primary_access_key
   sensitive = true
+}
+
+output "gpt4_deployment_name" {
+  value = azurerm_cognitive_deployment.gpt4_deployment.name
+}
+
+output "gpt4_model_version" {
+  value = azurerm_cognitive_deployment.gpt4_deployment.model[0].version
 }
