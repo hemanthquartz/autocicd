df82c43ebb74afff58951e970c53d9a3b28bc458
unknown <khemanthaws@gmail.com>
2025-02-19 20:45:59 -0500

    autocid

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_cognitive_account" "openai_account" {
   name                = "openaiaccount${random_id.unique.hex}"
   location            = "East US"
   resource_group_name = "openai_rg"
-  kind                = "OpenA"
+  kind                = "OpenAI"
   sku_name            = "S0"
 
   custom_subdomain_name = "openaiaccount${random_id.unique.hex}"
